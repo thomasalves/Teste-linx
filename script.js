538eb5c5e3a9783cce9e05a64b397dd86c27257b
Thomas Alves <thomasalves1111@gmai.com>
2020-08-22 01:29:42 -0300

    add produtos, compartilhe e footer

---
+++ script.js
@@ -0,0 +1,65 @@
+const API_URL = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products'
+
+const feachApi = async(url) => {
+    let response = await fetch(url)
+    const textResponse = await response.text()
+    return JSON.parse(textResponse)
+}
+
+const renderPage = async() => {
+    const dados = await feachApi(API_URL)
+    console.log(dados.products)
+
+
+    dados.products.map(aps => {
+    //console.log(aps.name)
+
+    const { name, image, oldPrice, price, description, installments } = aps;
+    console.log(aps)
+
+    row = document.getElementById("row")
+
+    card = document.createElement("div");
+    card.className = "col-md-4"
+
+    cardInfo = document.createElement("div");
+    cardInfo.className = "card-body"
+        // card.className.add("card", "mb-4", "boxshadow") Adiciona classes css
+
+
+    cardImg = document.createElement("img");
+    cardImg.className = "card-img"
+    cardImg.src = image
+
+    Name = document.createElement("p");
+    Name.className = "name";
+    Name.innerHTML = name;
+
+
+    oldP = document.createElement("p");
+    oldP.className = "oldPrice";
+    oldP.innerHTML = `De: R$${oldPrice.toFixed(2)}`;
+
+    Preco = document.createElement("p");
+    Preco.className = "price";
+    Preco.innerHTML = `Por: R$${price.toFixed(2)}`;
+
+    parcela = document.createElement("p");
+    parcela.className = "parcel";
+    parcela.innerHTML = `ou ${installments.count} de R$ ${installments.value.toFixed(2)}`
+
+    button = document.createElement("button")
+    button.className = "buying"
+    button.innerHTML = "Comprar"
+
+    row.appendChild(card)
+    card.appendChild(cardImg);
+    card.appendChild(cardInfo);
+    cardInfo.appendChild(Name)
+    cardInfo.appendChild(oldP)
+    cardInfo.appendChild(Preco)
+    cardInfo.appendChild(parcela)
+    cardInfo.appendChild(button)
+    })
+}
+renderPage()
